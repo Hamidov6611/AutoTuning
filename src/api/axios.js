6351f5327965406fc759a9177f625659f7ad5a4d
Hamidov6611 <dilmuroddelitot@gmail.com>
2024-02-10 09:57:02 +0500

    Auto tunning

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "http://185.225.34.216:3001/api",
 });
-export const BASE_URL = `http://localhost:3001/`;
-// export const BASE_URL = `http://185.225.34.216:3001/`;
+// export const BASE_URL = `http://localhost:3001/`;
+export const BASE_URL = `http://185.225.34.216:3001/`;
